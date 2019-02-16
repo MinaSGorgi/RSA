8f9bd5fdfcb65c16d8fcf4b6cbe7eb795a587994
Mahmoud Khaled Nasr <mahmoud.k.nasr@gmail.com>
2019-02-16 15:24:58 +0200

    Polish Fermat Math Attack and Added its graph class

--- src/main/java/rsa/attacks/FermatMathAttack.java
+++ src/main/java/rsa/attacks/FermatMathAttack.java
@@ -1,4 +1,4 @@
-package main.java.rsa.attacks;
+package attacks;
 
 import core.RSA;
 
@@ -13,9 +13,6 @@ public class FermatMathAttack {
         BigInteger k;
         BigInteger[] temp = modulus.sqrtAndRemainder();
         k = temp[1].equals(BigInteger.ZERO) ? temp[0] : temp[0].add(BigInteger.ONE);
-        if (k.pow(2).compareTo(modulus) <= 0){
-            System.out.println("stuck");
-        }
         BigInteger h;
         BigInteger reminder;
         BigInteger step = BigInteger.ZERO;
@@ -23,50 +20,32 @@ public class FermatMathAttack {
             BigInteger[] result = k.add(step).pow(2).subtract(modulus).sqrtAndRemainder();
             h = result[0];
             reminder = result[1];
-            if (h.pow(2).compareTo(modulus) >= 1){
-                System.out.println("Failed in the loop");
-            }
+            step = step.add(BigInteger.ONE);
         }while (! reminder.equals(BigInteger.ZERO));
-        BigInteger p = k.add(h), q = k.subtract(h);
 
-        if (!p.multiply(q).equals(modulus)){
-            System.out.println("Bug in the Fermat algorithm");
-        }
+        k = k.add(step).subtract(BigInteger.ONE);
+        BigInteger p = k.add(h), q = k.subtract(h);
 
         BigInteger phi = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
         privateKey = publicKey.modInverse(phi);
     }
 
-    BigInteger crack (BigInteger encryptedMessage){
+    public BigInteger crack (BigInteger encryptedMessage){
         return encryptedMessage.modPow(privateKey, modulus);
     }
 
     public static void main (String[] args) throws RSA.RSAException {
-        //BigInteger message = new BigInteger(args[0]);
+        BigInteger message = new BigInteger(args[0]);
         int nBits = Integer.parseInt(args[1]);
 
-        /*core.RSA rsa = new core.RSA(nBits);
+        core.RSA rsa = new core.RSA(nBits);
         BigInteger encrypted = rsa.encrypt(message);
-        System.out.println(rsa.toString());
 
         FermatMathAttack fermatMathAttack = new FermatMathAttack(rsa.publicKey, rsa.modulus);
         BigInteger cracked = fermatMathAttack.crack(encrypted);
 
         System.out.println("message = " + message);
         System.out.println("cracked = " + cracked);
-        System.out.println(cracked.equals(message) ?  "Success!" : "Failure!");*/
-
-        for (int i = 3; i < 10; i++) {
-            core.RSA rsa = new core.RSA(nBits);
-            BigInteger encrypted = rsa.encrypt(new BigInteger(String.valueOf(i)));
-            System.out.println(rsa.toString() + "\n" + i);
-            FermatMathAttack fermatMathAttack = new FermatMathAttack(rsa.publicKey, rsa.modulus);
-            BigInteger cracked = fermatMathAttack.crack(encrypted);
-
-            if (!cracked.equals(new BigInteger(String.valueOf(i)))){
-                System.out.println("Failure in cracking!");
-            }
-            System.out.println(i);
-        }
+        System.out.println(cracked.equals(message) ?  "Success!" : "Failure!");
     }
 }
---
+++ src/main/java/rsa/graphs/FermatMathAttackGraph.java
@@ -0,0 +1,74 @@
+package graphs;
+
+import javafx.application.Application;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.Scene;
+import javafx.scene.SnapshotParameters;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.image.WritableImage;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadMXBean;
+
+import core.RSA;
+import attacks.FermatMathAttack;
+
+import javax.imageio.ImageIO;
+
+
+public class FermatMathAttackGraph extends Application {
+    private final static int NUM_RUNS = 1000;
+    private final static int NANO_TO_MICRO = (int) Math.pow(10, 3);
+
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("Fermat Math Attack");
+        final NumberAxis xAxis = new NumberAxis();
+        final NumberAxis yAxis = new NumberAxis();
+        xAxis.setLabel("Key Size(bits)");
+        yAxis.setLabel("Crack Time(micro second)");
+        final LineChart<Number,Number> lineChart = new LineChart<>(xAxis,yAxis);
+
+        lineChart.setTitle("Fermat Math Attack");
+        XYChart.Series series = new XYChart.Series();
+        series.setName("Fermat Math Attack");
+
+        ThreadMXBean thread = ManagementFactory.getThreadMXBean();
+        for (int size = 32; size <= 80; ++size) {
+            System.out.println("Key Size: " + size);
+
+            RSA rsa = new RSA(size);
+
+            long start = thread.getCurrentThreadCpuTime();
+            new FermatMathAttack(rsa.publicKey, rsa.modulus);
+            long end = thread.getCurrentThreadCpuTime();
+            long duration = (end - start) / NUM_RUNS;
+
+            series.getData().add(new XYChart.Data(size, duration / NANO_TO_MICRO));
+        }
+
+        Scene scene  = new Scene(lineChart, 800, 600);
+        lineChart.setAnimated(false);
+        lineChart.getData().add(series);
+
+        stage.setScene(scene);
+        stage.show();
+
+        WritableImage image = scene.snapshot(null);
+        File file = new File("Fermat MathAttack.png");
+        try {
+            ImageIO.write(SwingFXUtils.fromFXImage(image, null), "png", file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
